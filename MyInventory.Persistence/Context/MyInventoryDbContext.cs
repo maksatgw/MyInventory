7a7e7b9e3bf56ea512089e8e9143944b15345c23
Maksat Oğuzhan Garyagdyyev <garyagdyyev@outlook.com>
2025-01-23 14:37:36 +0300

    feat: Add DbContext

---
+++ MyInventory.Persistence/Context/MyInventoryDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyInventory.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyInventory.Persistence.Context
+{
+    public class MyInventoryDbContext : DbContext
+    {
+        public MyInventoryDbContext(DbContextOptions<MyInventoryDbContext> options) : base(options) 
+        {
+        }
+        public DbSet<Assignment> Assignments { get; set; }
+        public DbSet<AssignmentHistory> AssignmentHistories { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Equipment> Equipments { get; set; }
+        public DbSet<EquipmentImage> EquipmentImages { get; set; }
+        public DbSet<Location> Locations { get; set; }
+    }
+}

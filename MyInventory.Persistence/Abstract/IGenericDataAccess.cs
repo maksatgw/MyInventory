c24e8e0f7d6c6aaa173593ee4594c58388d61759
Maksat Oğuzhan Garyagdyyev <garyagdyyev@outlook.com>
2025-01-23 15:09:14 +0300

    feat: Add GenericRepository abstract and concrete class

---
+++ MyInventory.Persistence/Abstract/IGenericDataAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyInventory.Persistence.Abstract
+{
+    public interface IGenericDataAccess<T> where T : class
+    {
+        public List<T> Get();
+        T Get(int id);
+        void Insert(T item);
+        void Update(T item);
+        void Delete(T item);
+    }
+}
---
+++ MyInventory.Persistence/Repository/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using MyInventory.Persistence.Abstract;
+using MyInventory.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyInventory.Persistence.Repository
+{
+    public class GenericRepository<T> : IGenericDataAccess<T> where T : class
+    {
+        private readonly MyInventoryDbContext _appDbContext;
+
+        public GenericRepository(MyInventoryDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public List<T> Get()
+        {
+            return _appDbContext.Set<T>().ToList();
+        }
+
+        public T Get(int id)
+        {
+            return _appDbContext.Set<T>().Find(id);
+        }
+
+        public void Insert(T item)
+        {
+            _appDbContext.Add(item);
+            _appDbContext.SaveChanges();
+        }
+
+        public void Update(T item)
+        {
+            _appDbContext.Update(item);
+            _appDbContext.SaveChanges();
+        }
+        public void Delete(T item)
+        {
+            _appDbContext.Remove(item);
+            _appDbContext.SaveChanges();
+        }
+    }
+}
+}

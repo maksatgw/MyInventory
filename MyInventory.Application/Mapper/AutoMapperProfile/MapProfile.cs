81803ba155787f1f7ca8723145c85a4727facdd9
Maksat Oğuzhan Garyagdyyev <garyagdyyev@outlook.com>
2025-01-25 18:12:36 +0300

    chore: Add AutoMapper config

---
+++ MyInventory.Application/Mapper/AutoMapperProfile/MapProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MyInventory.Application.DTO.EquipmentDtos;
+using MyInventory.Application.DTO.EquipmentImageDtos;
+using MyInventory.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyInventory.Application.Mapper.AutoMapperProfile
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Equipment, EquipmentDto>()
+           .ForMember(dest => dest.DateAdded, 
+                      opt => opt.MapFrom(src => src.DateAdded.ToShortDateString()))
+            .ForMember(dest => dest.DateUpdated,
+                       opt => opt.MapFrom(src => src.DateUpdated.ToShortDateString()));
+            CreateMap<EquipmentImage, EquipmentImageDto>();
+        }
+    }
+}

b2258a29c1e2ab85b61d0f738eb537e56f328886
Maksat Oğuzhan Garyagdyyev <garyagdyyev@outlook.com>
2025-01-25 18:12:01 +0300

    chore: Add Service and Data Access DI Containers

---
+++ MyInventory.API/Extensions/AppApplicationExtension.cs
@@ -0,0 +1,27 @@
+﻿using MyInventory.Application.Abstract;
+using MyInventory.Application.Concrete;
+using MyInventory.Application.Mapper.AutoMapperProfile;
+
+namespace MyInventory.API.Extensions
+{
+    public static class AppApplicationExtension
+    {
+        public static void AddApplictonServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(MapProfile).Assembly);
+
+            AddApplicationDIContainer(services);
+        }
+
+        public static void AddApplicationDIContainer(this IServiceCollection services)
+        {
+            services.AddScoped<IEquipmentService, EquipmentService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<ILocationService, LocationService>();
+            services.AddScoped<IAssignmentService, AssignmentService>();
+            services.AddScoped<IAssignmentHistoryService, AssignmentHistoryService>();
+            services.AddScoped<IEquipmentImageService, EquipmentImageService>();
+        }
+
+    }
+}
---
+++ MyInventory.API/Extensions/AppPersistenceExtension.cs
@@ -0,0 +1,24 @@
+﻿using MyInventory.Persistence.Entities.Repository;
+using MyInventory.Persistence.Entity.Abstract;
+using System.Runtime.CompilerServices;
+
+namespace MyInventory.API.Extensions
+{
+    public static class AppPersistenceExtension 
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            AddPersistenceDIContainer(services);
+        }
+        public static void AddPersistenceDIContainer(this IServiceCollection services) { 
+        
+            services.AddScoped<IEquipmentDataAccess, EquipmentRepository>();
+            services.AddScoped<ICategoryDataAccess, CategoryRepository>();
+            services.AddScoped<ILocationDataAccess, LocationRepository>();
+            services.AddScoped<IAssignmentDataAccess, AssignmentRepository>();
+            services.AddScoped<IAssignmentHistoryDataAccess, AssignmentHistoryRepository>();
+            services.AddScoped<IEquipmentImageDataAccess, EquipmentImageRepository>();
+            
+        }
+    }
+}

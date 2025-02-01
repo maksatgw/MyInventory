76e6adbb8b6e2a0dd4491b5a400cf7ffeb8eed5c
Maksat Oğuzhan Garyagdyyev <garyagdyyev@outlook.com>
2025-02-01 10:52:11 +0300

    feat: Added Pagination and filtering method for Equipment entity

--- MyInventory.Persistence/Entities/Abstract/IEquipmentDataAccess.cs
+++ MyInventory.Persistence/Entities/Abstract/IEquipmentDataAccess.cs
@@ -10,5 +10,20 @@ namespace MyInventory.Persistence.Entity.Abstract
 {
     public interface IEquipmentDataAccess : IGenericDataAccess<Equipment>
     {
+        /// <summary>
+        /// Retrieves a paginated, filtered, and searchable list of equipment asynchronously.
+        /// </summary>
+        /// <param name="pageNumber">The page number to retrieve (1-based index).</param>
+        /// <param name="pageSize">The number of equipment items per page.</param>
+        /// <param name="categoryId">Optional. Filters the equipment by the specified category ID.</param>
+        /// <param name="searchQuery">
+        /// Optional. Searches for equipment where the name or description contains the given query.
+        /// </param>
+        /// <returns>
+        /// A task representing the asynchronous operation that contains:
+        /// - A list of equipment for the specified page that matches the filters and search criteria.
+        /// - The total number of equipment items that match the given filters and search criteria.
+        /// </returns>
+        Task<(List<Equipment>, int)> GetPagedEquipmentsAsync(int pageNumber, int pageSize, int? categoryId = null, int? locationId = null, string? searchQuery = null);
     }
 }
--- MyInventory.Persistence/Entities/Repository/EquipmentRepository.cs
+++ MyInventory.Persistence/Entities/Repository/EquipmentRepository.cs
@@ -1,4 +1,5 @@
-﻿using MyInventory.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using MyInventory.Domain.Entities;
 using MyInventory.Persistence.Context;
 using MyInventory.Persistence.Entity.Abstract;
 using MyInventory.Persistence.Repository;
@@ -12,8 +13,40 @@ namespace MyInventory.Persistence.Entities.Repository
 {
     public class EquipmentRepository : GenericRepository<Equipment>, IEquipmentDataAccess
     {
+        private readonly MyInventoryDbContext _appDbContext;
         public EquipmentRepository(MyInventoryDbContext appDbContext) : base(appDbContext)
         {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<(List<Equipment>, int)> GetPagedEquipmentsAsync(int pageNumber, int pageSize, int? categoryId = null, int? locationId = null, string? searchQuery = null)
+        {
+            var query = _appDbContext.Equipments.AsQueryable();
+
+            if (categoryId.HasValue)
+            {
+                query = query.Where(e => e.CategoryId == categoryId.Value);
+            }
+
+            if (locationId.HasValue)
+            {
+                query = query.Where(e => e.LocationId == locationId.Value);
+            }
+
+            if (!string.IsNullOrWhiteSpace(searchQuery))
+            {
+                searchQuery.ToLower();
+                query = query.Where(e => e.Brand.ToLower().Contains(searchQuery) || e.Description.ToLower().Contains(searchQuery) || e.SerialNumber.ToLower().Contains(searchQuery) || e.Model.ToLower().Contains(searchQuery));
+            }
+
+            int totalItems = await query.CountAsync();
+            var items = await query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .Include(x => x.EquipmentImages)
+                .ToListAsync();
+
+            return (items, totalItems);
         }
     }
 }

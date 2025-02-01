2c7e821a4c6b28736ec1a7b88ca4b301e8e2a0c2
Maksat Oğuzhan Garyagdyyev <garyagdyyev@outlook.com>
2025-02-01 10:59:44 +0300

    config: Add ImgBB uploading service settings

---
+++ MyInventory.Infrastructure/Configurations/ImgBBSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyInventory.Infrastructure.Configurations
+{
+    public class ImgBBSettings
+    {
+        public string BaseUrl { get; set; }
+        public string ApiKey { get; set; }
+        public int Expiration { get; set; }
+    }
+}

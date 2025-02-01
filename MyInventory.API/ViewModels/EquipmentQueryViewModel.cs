167abfcdd5d44b620c53e74ea5871c3f4e042fbf
Maksat Oğuzhan Garyagdyyev <garyagdyyev@outlook.com>
2025-02-01 11:23:58 +0300

    feat: Add EquipmentQueryViewModel

---
+++ MyInventory.API/ViewModels/EquipmentQueryViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MyInventory.API.ViewModels
+{
+    public class EquipmentQueryViewModel
+    {
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public int? CategoryId { get; set; }
+        public int? LocationId { get; set; }
+        public string? SearchQuery { get; set; }
+    }
+}

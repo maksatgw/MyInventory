67b5da66e786c960cf2655065c968c5dcf006298
Maksat Oğuzhan Garyagdyyev <garyagdyyev@outlook.com>
2025-02-01 10:57:51 +0300

    docs: improve XML comments

--- MyInventory.Application/Abstract/IGenericService.cs
+++ MyInventory.Application/Abstract/IGenericService.cs
@@ -8,16 +8,50 @@ namespace MyInventory.Application.Abstract
 {
     public interface IGenericService<T> where T : class
     {
-        Task TDelete(T model);
-        Task TInsert(T model);
-        Task TUpdate(T model);
-        Task<T> TGet(int id);
+        /// <summary>
+        /// Deletes an existing record.
+        /// </summary>
+        /// <param name="model">The model representing the entity to be deleted.</param>
         Task Delete(TRead model);
+
+
+        /// <summary>
+        /// Deletes a list of records.
+        /// </summary>
+        /// <param name="modelList">The modelList representing the entities to be deleted </param>
         Task BulkDelete(List<TRead> modelList);
+
+        /// <summary>
+        /// Inserts a new record.
+        /// </summary>
+        /// <param name="model">The model representing the entity to be inserted.</param>
+        /// <returns>The inserted entity.</returns>
         Task<TRead> Insert(TCreate model);
+
+        /// <summary>
+        /// Updates an existing record.
+        /// </summary>
+        /// <param name="model">The model containing the updated values.</param>
         Task Update(TUpdate model);
+
+        /// <summary>
+        /// Retrieves a record by its unique identifier.
+        /// </summary>
+        /// <param name="id">The unique identifier of the entity.</param>
+        /// <returns>The entity matching the given identifier.</returns>
         Task<TRead> Get(int id);
+
+        /// <summary>
+        /// Retrieves list of records by their unique identifiers.
+        /// </summary>
+        /// <param name="idList">The list of unique identifiers.</param>
+        /// <returns>The matching entity list.</returns></returns>
         Task<List<TRead>> Get(List<int> idList);
+
+        /// <summary>
+        /// Retrieves all records.
+        /// </summary>
+        /// <returns>A list of all entities.</returns>
         Task<List<TRead>> Get();
     }
 }

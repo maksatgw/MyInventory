0bf345ae76e0fc23864c6eea5250d43c78cb5ce0
Maksat Oğuzhan Garyagdyyev <garyagdyyev@outlook.com>
2025-02-01 10:55:51 +0300

    feat: Add Generic DTO for paginated requests

---
+++ MyInventory.Application/DTO/PagedDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyInventory.Application.DTO
+{
+    public class PagedDto<T> where T : class
+    {
+        public int TotalItems { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public List<T> Items { get; set; }
+    }
+}

3fbcce6d058cca3482e19bc3681701a866173297
Maksat Oğuzhan Garyagdyyev <garyagdyyev@outlook.com>
2025-02-01 11:06:08 +0300

    feat: Add UploadImage method for imgBB service uploading, Add GetPagedEquipments method docs: improved XML comments

--- MyInventory.Application/Abstract/IEquipmentImageService.cs
+++ MyInventory.Application/Abstract/IEquipmentImageService.cs
@@ -1,4 +1,5 @@
-﻿using MyInventory.Domain.Entities;
+﻿using Microsoft.AspNetCore.Http;
+using MyInventory.Application.DTO.EquipmentImageDtos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,14 @@ using System.Threading.Tasks;
 
 namespace MyInventory.Application.Abstract
 {
-    public interface IEquipmentImageService : IGenericService<EquipmentImage>
+    public interface IEquipmentImageService : IGenericService<EquipmentImageDto, CreateEquipmentImageDto, UpdateEquipmentImageDto>
     {
+        /// <summary>
+        /// Upload image to imageHosting and save the url to the database
+        /// </summary>
+        /// <param name="file">Required Image</param>
+        /// <param name="equipmentId">Required EquipmentId</param>
+        /// <returns>CreateEquipmentDto to perform the insert operation</returns>
+        Task<CreateEquipmentImageDto> UploadImage(IFormFile file, int equipmentId);
     }
 }
--- MyInventory.Application/Abstract/IEquipmentService.cs
+++ MyInventory.Application/Abstract/IEquipmentService.cs
@@ -1,4 +1,6 @@
-﻿using MyInventory.Domain.Entities;
+﻿using MyInventory.Application.DTO;
+using MyInventory.Application.DTO.EquipmentDtos;
+using MyInventory.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +9,24 @@ using System.Threading.Tasks;
 
 namespace MyInventory.Application.Abstract
 {
-    public interface IEquipmentService : IGenericService<Equipment>
+    public interface IEquipmentService : IGenericService<EquipmentDto, CreateEquipmetDto, UpdateEquipmentDto>
     {
+        /// <summary>
+        /// Retrieves a paginated, filtered, and searchable list of equipment asynchronously.
+        /// </summary>
+        /// <param name="pageNumber">The page number to retrieve (1-based index).</param>
+        /// <param name="pageSize">The number of equipment items per page.</param>
+        /// <param name="categoryId">Optional. Filters the equipment by the specified category ID.</param>
+        /// <param name="locationId">Optional. Filters the equipment by the specified location ID.</param>
+        /// <param name="searchQuery">
+        /// Optional. Searches for equipment where the name or description contains the given query (case-insensitive).
+        /// </param>
+        /// <returns>
+        /// A task representing the asynchronous operation that contains a <see cref="PagedDto{EquipmentDto}"/>:
+        /// - A list of equipment DTOs for the specified page that match the filters and search criteria.
+        /// - The total number of equipment items that match the given filters and search criteria.
+        /// - Pagination metadata.
+        /// </returns>
+        Task<PagedDto<EquipmentDto>> GetPagedEquipments(int pageNumber, int pageSize, int? categoryId = null, int? locationId = null, string? searchQuery = null);
     }
 }
